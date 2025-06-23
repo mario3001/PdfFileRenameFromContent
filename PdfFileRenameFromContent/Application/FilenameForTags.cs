126b7bb1bd84eb6bd9269a9605d5e3c722bf840c
Mario Odenbach <mario.odenbach@abraxas.ch>
2025-06-23 09:16:18 +0200

    Added target folder by tags

--- PdfFileRenameFromContent/Application/FilenameForTags.cs
+++ PdfFileRenameFromContent/Application/FilenameForTags.cs
@@ -15,8 +15,22 @@ public class FilenameForTags
         var newFileName = new string(newFileNameChars.ToArray());
 
         var filenameByTags = $"{contentInformation.Date} {newFileName}{fileExtension}";
-        
+
         return CleanFileName(filenameByTags);
+    }
+
+    public string GetTargetFolderByTags(ContentInformation contentInformation)
+    {
+        var targetFolder = string.Join("", contentInformation.MatchingTagRules
+        .OrderBy(x => x.TagId)
+        .Select(x => x.TargetFolder));
+
+        if (string.IsNullOrEmpty(targetFolder))
+        {
+            return string.Empty;
+        }
+
+        return targetFolder;
     }
 
     private static string CleanFileName(string filename)
