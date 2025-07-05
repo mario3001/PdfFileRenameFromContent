3eae6165c0acc0faa145024287f58c0951f3e9e7
Mario Odenbach <commerce@odenba.ch>
2025-07-05 17:55:45 +0200

    chore: refactored publish script to create single-file

--- publish.executable.ps1
+++ publish.executable.ps1
@@ -5,10 +5,10 @@ $project = "PdfFileRenameFromContent.Executable"
 $configuration = "Release"
 $runtime = "win-x64" # Change as needed
 $outputDir = "publish/executable"
-$zipFile = "$project.zip"
+$zipFile = "publish/$project.zip"
 
 # Publish as self-contained
-dotnet publish $project/$project.csproj -c $configuration -r $runtime --self-contained true -o $outputDir
+dotnet publish $project/$project.csproj -c $configuration -r $runtime --self-contained true -o $outputDir -p:PublishSingleFile=true
 
 # Remove old zip if exists
 if (Test-Path $zipFile) {
